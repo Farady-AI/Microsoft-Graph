5fc7a0e4e1a8b9d084ce911a185d650a0de8468f
Farady-AI <josephmartuccijr@live.com>
2025-03-06 12:03:01 -0500

    Update main.go

--- main.go
+++ main.go
@@ -1,15 +1,3 @@
-module github.com/Farady-AI/Microsoft-Graph
-
-go 1.20
-
-require (
-    github.com/gin-gonic/gin v1.9.0
-    github.com/microsoftgraph/msgraph-sdk-go v1.0.0
-    github.com/spf13/viper v1.12.0
-)
-
-// main.go
-
 package main
 
 import (
