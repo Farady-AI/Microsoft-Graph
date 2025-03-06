62e682c8a8607c7f4c292c925532b9c4b1a0171a
Farady-AI <josephmartuccijr@live.com>
2025-03-06 14:17:10 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from dotenv import load_dotenv
 import os
 
 import requests
-import HTTPException
+
 print("DEBUG - ENV VARIABLES:")
 import pandas as pandas
 print("MSCLIENTID:", os.getenv("MSCLIENTID"))
